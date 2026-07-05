9bd6297dc593a65d6dd836ecde8cb18d000e0e19
Yongmin0423 <yym21345@gmail.com>
2026-07-05 21:55:46 +0900

    Your first mcp server

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import z from "zod";
+import { createMcpHandler } from "agents/mcp";
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
+		//mcp 서버 생성
+		const server = new McpServer({
+			name: "Stock server",
+			version: "1.0.0",
+		});
+
+		// 명세 정의
+		server.registerTool("get-stock-price", {
+			description: "Get the price of a stock given a ticker symbol.",
+			inputSchema: {
+				symbol: z.string().optional(),
+			},
+		}, async ({ symbol }) => {
+			const actualSymbol = symbol ?? "AAPL";
+			return {
+				content: [
+					{ type: "text", text: `The price of ${actualSymbol} is $123.45` }
+				]
+			}
+		})
+
+		const handler = createMcpHandler(server);
+		return handler(request, env, ctx);
 	},
 } satisfies ExportedHandler<Env>;
